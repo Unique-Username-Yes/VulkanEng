4125df354567b41ed466195d82e03e7d63d1976f
iLigonis <karmonasedvinas@gmail.com>
2019-06-13 22:30:06 +0100

    Update vulkan SDK; Implement validation layers

--- VulkanEngineTut/main.cpp
+++ VulkanEngineTut/main.cpp
@@ -5,10 +5,20 @@
 #include <stdexcept>
 #include <functional>
 #include <cstdlib>
+#include <cstring>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
+const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
 class TriangleApp
 {
 public:
@@ -24,6 +34,32 @@ private:
 	VkInstance instance;
 	GLFWwindow *window;
 
+	bool CheckValidationLayerSupport()
+	{
+		uint32_t layerCount = 0;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+		for (const char *layerName : validationLayers)
+		{
+			bool layerFound = false;
+			for (const auto &layerProperties : availableLayers)
+			{
+				if (strcmp(layerName, layerProperties.layerName) == 0)
+				{
+					layerFound = true;
+					break;
+				}
+			}
+
+			if (!layerFound)
+				return false;
+		}
+
+		return true;
+	}
 	void InitWindow()
 	{
 		glfwInit();
@@ -54,6 +90,22 @@ private:
 	}
 	void CreateInstance()
 	{
+		VkInstanceCreateInfo createInfo = {};
+
+		if (enableValidationLayers && !CheckValidationLayerSupport())
+		{
+			throw std::runtime_error("Validation layer requested, but not available.");
+		}
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
+
 		VkApplicationInfo appInfo = {};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "TriangleHi";
@@ -62,7 +114,7 @@ private:
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.apiVersion = VK_API_VERSION_1_0;
 
-		VkInstanceCreateInfo createInfo = {};
+
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
 
